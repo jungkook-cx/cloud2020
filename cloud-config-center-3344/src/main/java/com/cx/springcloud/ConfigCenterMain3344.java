9c76bd4706d72730f656db2fdf4594fe876e5a67
Xi.Chen <1351488703@qq.com>
2021-01-15 10:50:11 +0800

    'day2'

---
+++ cloud-config-center-3344/src/main/java/com/cx/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.cx.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @description:
+ * @author: ChenXi
+ * @time: 2021/1/14 15:54
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
