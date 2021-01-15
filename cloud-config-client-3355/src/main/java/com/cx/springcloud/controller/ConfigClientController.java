c53bfb37f9ccd8cd0687570e50bd4615eb87d0c2
Xi.Chen <1351488703@qq.com>
2021-01-15 10:48:23 +0800

    day2

---
+++ cloud-config-client-3355/src/main/java/com/cx/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,21 @@
+package com.cx.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: ChenXi
+ * @time: 2021/1/14 17:02
+ */
+@RestController
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
